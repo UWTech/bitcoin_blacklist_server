36176a64bd391ce23ba79d4347d04f98c39014ba
Eamon Maguire <edm6@uw.edu>
2020-06-30 20:56:00 -0700

    added basic health-check API
    added api logic with basic response for health-check endpoint

--- api.py
+++ api.py
@@ -5,11 +5,15 @@ contains the top level RESTFul API definitions
 import logging
 import global_variables
 from uvicorn.main import run
+from starlette.requests import Request
+from starlette.responses import JSONResponse, PlainTextResponse
 from starlette.applications import Starlette
 
 app = Starlette()
 
-
+@app.route("/api/v1/health", methods=["GET"])
+async def health(request: Request):
+    return PlainTextResponse('healthy', status_code=200)
 
 if __name__ == "__main__":
     run(app)
