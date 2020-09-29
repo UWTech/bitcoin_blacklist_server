2fe6da699866ea4a92fec9466b75f08640bcfaab
Eamon Maguire <edm6@uw.edu>
2020-09-28 19:44:56 -0700

    adding notes nad fixing API
    note: branch is corrupt. Try restarting from fresh master

--- blacklist/blacklist_request_handler.py
+++ blacklist/blacklist_request_handler.py
@@ -7,6 +7,7 @@ and permanent tables as appropriate
 '''
 
 import logging
+import subprocess
 import global_variables
 import ecdsa
 import ecies
@@ -100,7 +101,7 @@ class BlacklistRequestHandler:
         # avoid costly encryption if the record already exists, and return 'conflict'
         exists = self.check_for_blacklist_entry(pub_key_string)
         if exists:
-            return global_variables.CONFLICT_STRING, 'Record exists for public key'
+            return global_variables.CONFLICT_STRING, 'Record exists for public key', None
 
         # if not generate nonce
         # encrypt the nonce with the public key
@@ -176,7 +177,10 @@ class BlacklistRequestHandler:
             # if they do match, write a permanent blacklist request to the blacklist table
             try:
                 # TODO:: hash entry before writing: https://learnmeabitcoin.com/technical/public-key-hash
-                result = self.datastore_client.write_permanent_blacklist_record([pub_key])
+                # calculate the OP_HASH160 of the address, as this will be the scriptPubKey
+                # echo -n $PUB_KEY | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160
+                OP_HASH160 = subprocess.call('echo -n {} | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160'.format(pub_key))
+                result = self.datastore_client.write_permanent_blacklist_record(OP_HASH160)
                 return result
             except:
                 logging.exception('')
